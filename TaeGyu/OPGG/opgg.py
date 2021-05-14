d99d706027d0d6e816113a6bd5abe1600ec619ca
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-05-15 00:54:32 +0900

    find_champion_line 생성
    챔피언이 가는 라인을 찾아서 dict 형식으로 return 합니다.

--- TaeGyu/OPGG/opgg.py
+++ TaeGyu/OPGG/opgg.py
@@ -63,6 +63,8 @@ class OPGG():
 
         Returns:
             [list]]: 해당 라인의 챔피언 티어정보
+                     아래와 같은 형식으로 return 합니다.
+                [['top', 'Shen', '1'], ['top', 'Aatrox', '1'], ... ]
         """
 
         # 결과 저장 리스트
@@ -72,25 +74,25 @@ class OPGG():
         html = self.read_html(self.OPGG_URL)
         
         ## 티어 챔피언 정보 tag 리스트
-        champion_info = html.find("tbody", class_='tabItem champion-trend-tier-{}'.format(line)
-                           ).find_all('tr')
+        champion_info_list= html.find("tbody", class_='tabItem champion-trend-tier-{}'.format(line)
+                               ).find_all('tr')
 
-        for n in range(len(champion_info)):
+        for n in range(len(champion_info_list)):
 
             # line 찾기
-            line_position = champion_info[n].find("a"
-                                           ).attrs["href"
-                                           ].split("/")[-1]
+            line_position = champion_info_list[n].find("a"
+                                                ).attrs["href"
+                                                ].split("/")[-1]
 
             # champion_name 찾기
-            champion_name = champion_info[n].find("div", class_="champion-index-table__name"
-                                           ).get_text()
+            champion_name = champion_info_list[n].find("div", class_="champion-index-table__name"
+                                                ).get_text() 
 
             # tier 찾기
-            tier = champion_info[n].find_all("img")[-1
-                                  ].attrs["src"
-                                  ].split("-")[-1
-                                  ].split(".")[0]
+            tier = champion_info_list[n].find_all("img")[-1
+                                       ].attrs["src"
+                                       ].split("-")[-1
+                                       ].split(".")[0]
 
             # 결과 list에 저장
             result_list.append([line_position, champion_name, tier])
@@ -107,6 +109,8 @@ class OPGG():
 
         Returns:
             [list]: 모든 라인의 티어 list
+                    아래와 같은 형식으로 return 합니다.
+                [['top', 'Shen', '1'], ['top', 'Aatrox', '1'], ... ]
         """
 
         
@@ -129,14 +133,46 @@ class OPGG():
         return result_list
 
 
+    def find_champion_line(self):
+        """챔피언이 가는 라인을 찾아서
+           dict 형식으로 return 합니다.
+
+        Returns:
+            [dict]]: 아래와 같은 형식으로 리턴 합니다.
+                { 'Aatrox': ['Top'], 'Ahri': ['Middle'], ... }
+        """
+  
+        # 결과 저장 리스트
+        result_dict = {}
+
+        ## html 읽기
+        html = self.read_html(self.OPGG_URL)
+        
+        ## 티어 챔피언 정보 tag 리스트
+        champion_info_list = html.find("div", class_="champion-index__champion-list").find_all("a")
 
 
-            
-    
+        for n in range(len(champion_info_list)):
+
+            lt = []
+
+            champion_name = champion_info_list[n].find("div", class_="champion-index__champion-item__name"
+                                                 ).get_text()
+
+            champion_line = champion_info_list[n].find("div", class_="champion-index__champion-item__positions"
+                                                 ).find_all("div", class_="champion-index__champion-item__position")
+            # 챔피언의 라인 넣기
+            # 1개 이상이 나올 수 있어서 전부 찾아줌
+            for cl in champion_line:
+                lt.append(cl.get_text())
+
+            result_dict[champion_name] = lt
+
+        return result_dict
 
 if __name__ == '__main__':
 
     a = OPGG()
-    c = a.find_champion_tier_all()
-    print(c)
-    # a.find_champion_line()
+    # c = a.find_champion_tier_all()
+    # print(c)
+    print(a.find_champion_tier("TOP"))
