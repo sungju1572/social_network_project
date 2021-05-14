7d52b2c388506ed6cd44027cb9b7e56aadac8d6a
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-05-14 23:30:16 +0900

    find_tier 함수 제작
    원하는 챔피언의 라인을 받아서 그 라인의 티어 list return 합니다.

--- TaeGyu/OPGG/opgg.py
+++ TaeGyu/OPGG/opgg.py
@@ -4,9 +4,7 @@
     20171483 한태규
     
     ------------------------- memo -------------------------
-    
-    
-    
+
     --------------------------------------------------------
      
     email : gksxorb147@naver.com
@@ -17,9 +15,14 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
+
+
 class OPGG():
 
     def __init__(self):
+
+        self.OPGG_URL = "https://www.op.gg/champion/statistics"
+
         pass
 
     def read_html(self, URL):
@@ -37,15 +40,74 @@ class OPGG():
             req = requests.get(URL)
             html = req.text
             soup = BeautifulSoup(html, 'html.parser')
-            print(type(soup))
             return soup
+
         except:
             print("read_html ERROR")
+
+
+
+
+    def find_tier(self, line="TOP"):
+        """원하는 챔피언의 라인을 받아서 
+           그 라인의 티어 list를
+           return 합니다. 
+
+        Args:
+            line (str, optional): Defaults to "TOP".
+                 원하는 라인을 입력합니다.
+            
+            들어가야할 인자. : 
+                 (TOP, JUNGLE, MID, ADC, SUPPORT)
+
+        Returns:
+            [list]]: 해당 라인의 챔피언 티어정보
+        """
+
+        # 결과 저장 리스트
+        result_list = []
+
+        ## html 읽기
+        html = self.read_html(self.OPGG_URL)
+        
+        ## 티어 챔피언 정보 tag 리스트
+        champion_info = html.find("tbody", class_='tabItem champion-trend-tier-{}'.format(line)
+                           ).find_all('tr')
+
+        for n in range(len(champion_info)):
+
+            # line 찾기
+            line_position = champion_info[n].find("a"
+                                           ).attrs["href"
+                                           ].split("/")[-1]
+
+            # champion_name 찾기
+            champion_name = champion_info[n].find("div", class_="champion-index-table__name"
+                                           ).get_text()
+
+            # tier 찾기
+            tier = champion_info[n].find_all("img")[-1
+                                  ].attrs["src"
+                                  ].split("-")[-1
+                                  ].split(".")[0]
+
+            # 결과 list에 저장
+            result_list.append([line_position, champion_name, tier])
+
+        # type : list
+        return result_list
+
+    
+
+    
+
+
+
 
 
 if __name__ == '__main__':
 
     a = OPGG()
-    b = a.read_html("https://www.op.gg/champion/statistics")
-    # print(b)
+    c = a.find_tier("JUNGLE")
+    print(c)
     
